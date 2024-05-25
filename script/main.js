1ccf6163e796e608f07d0ec2cb426c82cf6e1bc4
suriya216 <sssuriya003@gmail.com>
2024-05-25 07:12:27 -0700

    dark and light mode

---
+++ script/main.js
@@ -0,0 +1,15 @@
+const modeToggle = document.getElementById('pressed');
+
+let isDarkMode = false;
+
+modeToggle.addEventListener('click', function() {
+    isDarkMode = !isDarkMode;
+    if (isDarkMode) {
+        document.body.classList.add('dark_mode');
+        modeToggle.src='assets/mode_dark.png';
+    } else {
+        document.body.classList.remove('dark_mode');
+        modeToggle.src='assets/mode_light.png';
+    }
+    modeToggle.style.display = 'inline-block';
+});
