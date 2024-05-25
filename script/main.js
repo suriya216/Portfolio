8d1bdf9750f0df7e270861123cf5ed7a19753795
suriya216 <sssuriya003@gmail.com>
2024-05-25 07:42:45 -0700

    dark/night mode

--- script/main.js
+++ script/main.js
@@ -6,10 +6,10 @@ modeToggle.addEventListener('click', function() {
     isDarkMode = !isDarkMode;
     if (isDarkMode) {
         document.body.classList.add('dark_mode');
-        modeToggle.src='assets/mode_dark.png';
+        modeToggle.src='assets/mode_light.png';
     } else {
         document.body.classList.remove('dark_mode');
-        modeToggle.src='assets/mode_light.png';
+        modeToggle.src='assets/mode_dark.png';
     }
     modeToggle.style.display = 'inline-block';
 });
