d82a2334c91ed86b7f8f52597a750bc963200fa0
suriya216 <sssuriya003@gmail.com>
2024-05-29 22:23:25 +0530

    final commit

--- script/resume.js
+++ script/resume.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function(){
     downloadBtn.addEventListener("click", function(){
         var link=document.createElement("a");
 
-        link.href="documents\SURIYA 717821P159.pdf";
+        link.href="documents\\717821P159 SURIYA S.pdf";
         link.download="resume.pdf";
 
         document.body.appendChild(link);
